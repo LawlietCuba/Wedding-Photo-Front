fe6a1d5cd7371a2a88cf03a4975aff960476c581
LawlietCuba <rodrigao.mederos@gmail.com>
2025-03-02 18:36:22 -0500

    Created Navbar, added the logo and added new typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { PhotoComponent } from './components/photoComponent/photo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, PhotoComponent],
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
